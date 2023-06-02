595474f78d3b0a1af0667b1aca1856d232305ed1
Elvis O <elvis.osujic@gmail.com>
2023-06-02 14:14:11 +0100

    Complete.

--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,49 @@
 var express = require("express");
 var router = express.Router();
+const User = require("../model/user");
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.send("Home page nigga!");
+  res.send("Home page!");
 });
 
-router.post("/", (req, res) => {
+router.post("/", async (req, res) => {
   // console.log(req.body);
   const { phoneNumber, text } = req.body;
   console.log(text);
   let response;
 
   if (text === "") {
-    response = `CON Enter your first name`;
+    response = `CON Enter your full name`;
   }
   if (text !== "") {
-    response = `CON Enter your phone number`;
+    let textArray = text.split("*");
+    console.log(textArray);
+
+    if (textArray.length === 1) {
+      response = `CON Enter your Phone number`;
+    } else if (textArray.length === 2) {
+      if (parseInt(textArray[1]) > 0) {
+        response = `CON Please confirm if you want to save the details\n1. Confirm\n2. Cancel`;
+      } else {
+        response = `END Please try again and make sure your Phone number is valid`;
+      }
+    } else if (textArray.length === 3) {
+      if (parseInt(textArray[2]) === 1) {
+        const fullName = textArray[0];
+        const number = textArray[1];
+        const user = new User({
+          fullName,
+          number,
+        });
+        await user.save();
+        response = `END Your data was saved successfully!`;
+      } else if (parseInt(textArray[2]) === 2) {
+        response = `END Data was not saved. `;
+      } else {
+        response = `END Invalid Input. Please try again`;
+      }
+    } else response = `END Network Error, Please try again.`;
   }
 
   setTimeout(() => {
@@ -26,3 +53,9 @@ router.post("/", (req, res) => {
 });
 
 module.exports = router;
+
+// response =
+// `END Your fullname is ` +
+// textArray[0] +
+// ` and your Phone number is ` +
+// textArray[1];
